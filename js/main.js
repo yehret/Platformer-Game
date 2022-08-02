9781f4f31b67b24048198c0b48f51d9aeb63efde
Serhii <sirozhka.v@gmail.com>
2022-08-02 16:18:15 +0200

    Add images and death pits

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-import platform from './img/platform'
+// Images
 
-const hills = new Image()
+let platformImg = new Image()
+platformImg.src = './img/platform.png'
+
+let platformSmallImg = new Image()
+platformSmallImg.src = './img/platformSmallTall.png'
+
+let hills = new Image()
 hills.src = './img/hills.png'
 
-const background = new Image()
+let background = new Image()
 background.src = './img/background.png'
+
+// --------------------------------------------------
 
 
 const canvas = document.querySelector('canvas')
@@ -18,6 +26,7 @@ const gravity = 1
 
 class Player {
     constructor() {
+        this.speed = 5
         this.position = {
             x: 100,
             y: 100
@@ -28,6 +37,7 @@ class Player {
             x: 0,
             y: 0
         }
+        this.direction = 1
     }
     draw() {
         c.fillStyle = 'red'
@@ -41,7 +51,6 @@ class Player {
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
             this.velocity.y += gravity
-        else this.velocity.y = 0
     }
 }
 
@@ -55,12 +64,17 @@ class Platform {
         this.image = image
         this.width = image.width
         this.height = image.height
+        // this.width = 300
+        // this.height = 20
     }
     
     draw() {
         c.drawImage(this.image, this.position.x, this.position.y)
+        // c.fillStyle = 'blue'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
+
 
 class GenericObject {
     constructor({ x, y, image }) {
@@ -85,26 +99,45 @@ class GenericObject {
 //     return image
 // }
 
-const platformImg = new Image()
-platformImg.src = 'img/platform.png'
-
-const player = new Player()
+let player = new Player()
 // const platform = new Platform()
-const platforms = [new Platform({
-    x: -1, 
-    y: 470,
-    image: platformImg}),
-     new Platform({x: image.width - 3, y: 470, image: platformImg})]
+let platforms = [
+    // new Platform({
+    // x: -1, 
+    // y: 470,
+    // image: platformImg
+    // }),
+    // new Platform({
+    // x: platformImg.width - 3,
+    // y: 470,
+    // image: platformImg
+    // }),
+    // new Platform({
+    // x: platformImg.width *2 + 100,
+    // y: 470,
+    // image: platformImg
+    // }),
+]
 
-// const GenericObject = [
-//     new GenericObject({
-//         x: 0,
-//         y: 0,
-//         image: background
-//     })
-// ]
+let genericObject = [
+    // new GenericObject({
+    //     x: -1,
+    //     y: -1,
+    //     image: background
+    // }),
+    // new GenericObject({
+    //     x: -1,
+    //     y: -1,
+    //     image: hills
+    // })
+]
+
+
 
 const keys = {
+    space: {
+        pressed: false
+    },
     right: {
         pressed: false
     },
@@ -115,10 +148,74 @@ const keys = {
 
 let scrollOffset = 0
 
+function init(){
+
+player = new Player()
+// const platform = new Platform()
+platforms = [
+    new Platform({
+    x: platformImg.width *4 + 300 - 3 + platformImg.width - platformSmallImg.width,
+    y: 350,
+    image: platformSmallImg
+    }),
+    new Platform({
+    x: -1, 
+    y: 470,
+    image: platformImg
+    }),
+    new Platform({
+    x: platformImg.width - 3,
+    y: 470,
+    image: platformImg
+    }),
+    new Platform({
+    x: platformImg.width *2 + 100,
+    y: 470,
+    image: platformImg
+    }),
+    new Platform({
+    x: platformImg.width *3 + 300,
+    y: 470,
+    image: platformImg
+    }),
+    new Platform({
+    x: platformImg.width *4 + 300 - 3,
+    y: 470,
+    image: platformImg
+    }),
+    new Platform({
+    x: platformImg.width *5 + 600 - 3,
+    y: 470,
+    image: platformImg
+    }),
+]
+
+genericObject = [
+    new GenericObject({
+        x: -1,
+        y: -1,
+        image: background
+    }),
+    new GenericObject({
+        x: -1,
+        y: -1,
+        image: hills
+    })
+]
+
+
+    scrollOffset = 0
+
+}
+
 function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
+
+    genericObject.forEach(genericObject => {
+        genericObject.draw()
+    })
 
     platforms.forEach(platform => {
         platform.draw()
@@ -126,21 +223,33 @@ function animate() {
     player.update()
 
     if (keys.right.pressed && player.position.x < 400) {
-        player.velocity.x = 5
-    } else if (keys.left.pressed && player.position.x > 100){
-        player.velocity.x = -5
+        player.velocity.x = player.speed
+    } else if (
+        (keys.left.pressed && player.position.x > 200) || 
+        (keys.left.pressed && scrollOffset === 0 && player.position.x > 0)
+    ){
+        player.velocity.x = -player.speed
     } else {
         player.velocity.x = 0
 
         if (keys.right.pressed) {
-            scrollOffset += 5
+            scrollOffset += player.speed
             platforms.forEach(platform => {
-                platform.position.x -= 5
+                platform.position.x -= player.speed
             })
-        } else if (keys.left.pressed) {
-            scrollOffset -= 5
+
+            genericObject.forEach(genericObject => {
+                genericObject.position.x -= player.speed * 0.66
+            })
+        } else if (keys.left.pressed && scrollOffset > 0) {
+            scrollOffset -= player.speed
+
             platforms.forEach(platform => {
-                platform.position.x += 5
+                platform.position.x += player.speed
+            })
+
+            genericObject.forEach(genericObject => {
+                genericObject.position.x += player.speed * 0.66
             })   
         }
     }
@@ -156,15 +265,24 @@ function animate() {
         }
     })
 
-    if (scrollOffset > 2000) {
+    console.log(scrollOffset)
+
+    // win
+    if (scrollOffset > platformImg.width *5 + 500 - 3) {
         console.log('You Win!');
+    }
+
+    // lose
+    if (player.position.y > canvas.height) {
+        init()
     }
 }
 
-animate();
+init()
+animate()
 
 addEventListener('keydown', ({ keyCode }) => {
-    // console.log(keyCode)d
+    // console.log(keyCode)
     switch (keyCode) {
         case 65:
             console.log('left')
@@ -179,7 +297,10 @@ addEventListener('keydown', ({ keyCode }) => {
             break
         case 87:
             console.log('up')
-            player.velocity.y -= 20
+            player.velocity.y -= 25
+            break
+        case 32:
+            console.log('space')
             break
     }
 })
@@ -198,9 +319,8 @@ addEventListener('keyup', ({ keyCode }) => {
             console.log('right')
             keys.right.pressed = false
             break
-        // case 87:
-        //     console.log('up')
-        //     player.velocity.y -= 20
-        //     break
+        case 87:
+            console.log('up')
+            break
     }
 })
